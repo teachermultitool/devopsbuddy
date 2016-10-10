1b39b4e014893d97e87aaad3dcd9ac966eb5e3b2
Alexander Redmann <redmann@xplace.de>
2016-10-10 13:41:12 +0200

    Add index controller and index html

--- src/main/java/de/redmann/test/web/controllers/IndexController.java
+++ src/main/java/de/redmann/test/web/controllers/IndexController.java
@@ -1,7 +1,17 @@
 package de.redmann.test.web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by redmann on 10.10.16.
  */
-public class IndexController {
+@Controller
+public class IndexController
+{
+	@RequestMapping ("/")
+	public String home()
+	{
+		return "index";
+	}
 }
