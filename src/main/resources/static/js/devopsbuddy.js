fb9619a0ac1e08a20b56ab510d35636174d1801b
Alexander Redmann <redmann@xplace.de>
2016-10-27 18:22:46 +0200

    add formvalidation

--- src/main/resources/static/js/devopsbuddy.js
+++ src/main/resources/static/js/devopsbuddy.js
@@ -11,4 +11,100 @@ function main() {
         $collapse.collapse('toggle');
     });
 
+    $('#contactForm').formValidation({
+        framework: 'bootstrap',
+        icon: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'The email is required'
+                    },
+                    emailAddress: {
+                        message: 'The input is not a valid email address'
+                    }
+                }
+            },
+            firstName: {
+                validators: {
+                    notEmpty: {
+                        message: 'The first name is required'
+                    }
+                }
+            },
+            lastName: {
+                validators: {
+                    notEmpty: {
+                        message: 'The last name is required'
+                    }
+                }
+            },
+            feedback: {
+                validators: {
+                    notEmpty: {
+                        message: 'The feedback is required'
+                    }
+                }
+            }
+        }
+    });
+
+    $('#forgotPasswordForm').formValidation({
+        framework: 'bootstrap',
+        icon: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'The email is required'
+                    },
+                    emailAddress: {
+                        message: 'The input is not a valid email address'
+                    }
+                }
+            }
+        }
+    });
+
+    $('#savePasswordForm').formValidation({
+        framework: 'bootstrap',
+        icon: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: 'The password is required'
+                    },
+                    identical: {
+                        field: 'confirmPassword',
+                        message: 'The password and its confirmation is not the same'
+                    }
+                }
+            },
+            confirmPassword: {
+                validators: {
+                    notEmpty: {
+                        message: 'The confirmation password is required'
+                    },
+                    identical: {
+                        field: 'password',
+                        message: 'The password and its confirmation is not the same'
+                    }
+                }
+            }
+        }
+    });
+
 }
